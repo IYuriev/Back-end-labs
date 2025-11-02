df06f2bd8eac43483256ab00885d035d149cb7e8
illiayuriev-spec <illia.yuriev@skelar.tech>
2025-11-02 17:39:41 +0200

    update user module to use async methods and enhance user entity

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,9 +1,27 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+} from 'typeorm';
+import { Account } from '../account/account.entity';
+
+@Entity('users')
 export class User {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ type: 'varchar', length: 255 })
   name: string;
 
-  constructor(id: string, name: string) {
-    this.id = id;
-    this.name = name;
-  }
+  @OneToOne(() => Account, (account) => account.user, { cascade: true })
+  account: Account;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
 }
